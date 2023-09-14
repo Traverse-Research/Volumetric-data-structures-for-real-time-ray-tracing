cd93ef325860af213beee3eafaf04c2ea938b805
rosaliedewinther <rosaliedewinther@gmail.com>
2023-09-14 17:35:46 +0200

    Added all required figures for the doc

--- compare_images_pytorch/run_comparison.py
+++ compare_images_pytorch/run_comparison.py
@@ -19,6 +19,25 @@ to_compare = {
     "fire_format_f16.png": "fire_format_f32.png",
     "fire_format_unorm.png": "fire_format_f32.png",
     "fire_format_bc7.png": "fire_format_f32.png",
+
+    "disney_cloud_clustering_100_5_1.png": "disney_cloud_clustering_raw.png",
+    "disney_cloud_clustering_100_5_01.png": "disney_cloud_clustering_raw.png",
+    "disney_cloud_clustering_100_5_99.png": "disney_cloud_clustering_raw.png",
+    "disney_cloud_clustering_100_10_1.png": "disney_cloud_clustering_raw.png",
+    "disney_cloud_clustering_1000_5_1.png": "disney_cloud_clustering_raw.png",
+
+    "dragon_clustering_100_5_1.png": "dragon_clustering_raw.png",
+    "dragon_clustering_100_5_01.png": "dragon_clustering_raw.png",
+    "dragon_clustering_100_5_99.png": "dragon_clustering_raw.png",
+    "dragon_clustering_100_10_1.png": "dragon_clustering_raw.png",
+    "dragon_clustering_1000_5_1.png": "dragon_clustering_raw.png",
+
+    "shockwave_clustering_100_5_1.png": "shockwave_clustering_raw.png",
+    "shockwave_clustering_100_5_01.png": "shockwave_clustering_raw.png",
+    "shockwave_clustering_100_5_99.png": "shockwave_clustering_raw.png",
+    "shockwave_clustering_100_10_01.png": "shockwave_clustering_raw.png",
+    "shockwave_clustering_1000_5_01.png": "shockwave_clustering_raw.png",
+
 }
 
 
@@ -29,16 +48,9 @@ for comp, base in to_compare.items():
     
     diff_maker = nn.MSELoss(reduction='none')
     diff = torch.sqrt(diff_maker(comp_im, base_im))
-    print(torch.mean(diff))
-    print(torch.median(diff))
-    print(torch.min(diff))
-    print(torch.max(diff))
-    print(diff.shape)
-    to_write_im = torch.reshape(torch.mean(diff, 0), (1,1400, 2248))
+    to_write_im = torch.reshape(torch.mean(diff, 0), (1,diff.shape[1], diff.shape[2]))
     to_write_im = to_write_im*10
-    print(to_write_im.shape)
     to_write_im = to_write_im.repeat(3,1,1)
-    print(to_write_im.shape)
     #to_write_im = torch.lerp(to_write_im, end, 1)
     torchvision.io.write_png(functional.convert_image_dtype(to_write_im, torch.uint8), "diff_" + comp)
 
